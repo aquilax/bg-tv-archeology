34fffc5924f3c4628939c7460429ca2ef608f3b9
aquilax <aquilax@gmail.com>
2022-12-09 16:53:02 +0100

    Separated common config in common.php

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,43 +1,6 @@
 <?php
 
-$tv = [
-  'parva-programa' => [
-    'name' => 'Първа програма',
-    'from' => '1959-12-26',
-    'to' => '1992-05-31',
-    'lineage' => 'parva-programa',
-  ],
-  'kanal-1' => [
-    'name' => 'Канал 1',
-    'from' => '1992-06-01',
-    'to' => '2008-08-13',
-    'lineage' => 'parva-programa',
-  ],
-  'bnt-1' => [
-    'name' => 'БНТ 1',
-    'from' => '2008-08-14',
-    'to' => '',
-    'lineage' => 'parva-programa',
-  ],
-  'vtora-programa' => [
-    'name' => 'Втора програма',
-    'from' => '1975-09-09',
-    'to' => '1992-05-31',
-    'lineage' => 'vtora-programa',
-  ],
-  'efir-2' => [
-    'name' => 'Ефир 2',
-    'from' => '1992-06-01',
-    'to' => '2000-05-31',
-    'lineage' => 'vtora-programa',
-  ],
-  'bnt-2' => [
-    'name' => 'БНТ 2',
-    'from' => '2011-10-16',
-    'to' => '',
-    'lineage' => 'vtora-programa',
-  ],
-];
+require('common.php');
 
 $startDate = '1959-12-26';
 $endDate = '2001-01-01';
